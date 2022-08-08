440104889edbc7b04c75469865874c92d76aac26
Drew Dru <drew.drux@gmail.com>
2022-08-08 10:57:20 +0300

    change home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Main } from "components";
+
+const Home: React.FC = () => {
+  return (
+    <div className="d-flex flex-column min-vh-100">
+      <Main />
+    </div>
+  );
+};
+
+export default Home;
